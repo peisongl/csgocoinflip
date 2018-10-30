b8e457643a97d6a2e35f3aff3336696cae40abea
Peisong <peisong@Peisongs-MacBook-Pro.local>
2018-10-30 02:00:31 -0400

    rename users

--- server/models/users.js
+++ server/models/users.js
@@ -1,32 +1,28 @@
-t mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-name: {
+  name: {
     type: String,
-        required: [true, 'Name fields is required'],
-            unique: true
-              
-},
-password: {
+    required: [true, 'Name fields is required'],
+    unique: true
+  },
+  password: {
     type: String
-      
-},
-created_at: {
+  },
+  created_at: {
     type: Date,
-        required: true,
-            default: Date.now()
-              
-},
+    required: true,
+    default: Date.now()
+  },
   avatar_url: String,
-    email: String,
-      followers: Number,
-        following: Number,
-          role: String,
-            gender: String
+  email: String,
+  followers: Number,
+  following: Number,
+  role: String,
+  gender: String
+})
 
-        })
+const User = mongoose.model('User', UserSchema)
 
-        const User = mongoose.model('User', UserSchema)
-
-        module.exports = User
+module.exports = User
